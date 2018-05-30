deed36ecad34253a247e05c1218c4e191a5be966
vinotha64 <vinothablossom14@gmail.com>
2018-05-30 15:18:55 +0530

    sample

---
+++ operator/src/basic/mystery3.java
@@ -0,0 +1,28 @@
+package basic;
+
+public class mystery3 
+	{
+	public static void main( String[] args )
+	{
+	int row = 10;
+	int column;
+
+	while ( row >= 1 )
+	 {
+	 column = 1;
+
+	 while ( column <= 10 )
+	 {
+	 System.out.print( row % 2 == 1 ? "<" : ">" );
+	 ++column;
+	 } 
+
+	 --row;
+	 System.out.println();
+	 }
+	 } 
+	 }
+
+
+
+
