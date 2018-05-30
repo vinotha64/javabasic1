fff6b15f45d0c02f9f4fcd360ebef3cf16060eb2
vinotha64 <vinothablossom14@gmail.com>
2018-05-30 15:08:26 +0530

    c

---
+++ operator/src/basic/mystery2.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class mystery2 {
+	public static void main( String[] args )
+	{
+	int count = 1;
+
+	while ( count <= 10 )
+	{
+	 System.out.println( count % 2 == 1 ? "****" : "++++++++" );
+	 ++count;
+	 } 
+	}
+	 } 
+
