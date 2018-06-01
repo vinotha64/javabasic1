ed6d191a4820f453c3b23920d613d387d7b5c212
vinotha64 <vinothablossom14@gmail.com>
2018-06-01 10:51:35 +0530

    celsius

--- operator/src/basic/Multiple.java
+++ operator/src/basic/Multiple.java
@@ -8,10 +8,32 @@ public boolean ismultiple(int one,int second)
 	else
 		return false;
 }
+public boolean iseven(int second)
+{
+	if(second%2==0)
+		return true;
+	else
+		return false;
+}
+public double celsius(double farenheit){
+	double celsius= 5.0 / 9.0 * ( farenheit - 32 );
+	return celsius;
+}
+public double farenheit(double celsius) {
+	double farenheit=9.0 / 5.0 * celsius + 32;
+	return farenheit;
+}
+		
+
 public static void main(String[] args) {
 	Multiple m1= new Multiple();
    m1.ismultiple(6,3);
-   System.out.println(m1.ismultiple(2,6));
+   m1.celsius(98);
+   m1.farenheit(108);
+   System.out.println(m1.farenheit(108));
+   System.out.println(m1.celsius(98));
+System.out.println(m1.ismultiple(2,6));
+System.out.println(m1.iseven(3));
 }
 
 
