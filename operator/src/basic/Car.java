729c7708a9286a947a718afbfa81c75447fe0f06
vinotha64 <vinothablossom14@gmail.com>
2018-06-05 18:39:49 +0530

    car

---
+++ operator/src/basic/Car.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class Car {
+	public void brake() {
+		int i=10;
+		System.out.println("cars brake"+i);
+	}
+
+public static void main(String[] args) {
+	
+	Car car1=new Car();
+	car1.brake();
+}
+}
