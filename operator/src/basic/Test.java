7fc76a18779ebcd094abfd6b9b6dad567bec1444
vinotha64 <vinothablossom14@gmail.com>
2018-06-07 15:17:29 +0530

    animal

---
+++ operator/src/basic/Test.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class Test {
+	public static void main(String[] args) {
+	Travel travel1=new bus();
+	Travel travel2=new dog();
+	System.out.println("travel1 instanceof bus");
+	System.out.println("travel2 instanceof dog");
+		
+	}
+
+}
---
+++ operator/src/basic/Vehicle.java
@@ -0,0 +1,34 @@
+package basic;
+
+public abstract class Vehicle implements Travel{
+}
+
+	class cycle extends Vehicle{
+
+		@Override
+		public void getspeed() {
+		
+			
+		}
+		
+	}
+
+	class lorry extends Vehicle{
+
+		@Override
+		public void getspeed() {
+		
+			
+		}
+		
+	}
+	class bus extends Vehicle{
+
+		@Override
+		public void getspeed() {
+		
+			
+		}
+		
+	}
+
