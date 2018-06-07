020e77c48d6249c344057ad0f2ce07ef98ae0c49
vinotha64 <vinothablossom14@gmail.com>
2018-06-07 10:01:39 +0530

    abstract

---
+++ operator/src/basic/Abc.java
@@ -0,0 +1,24 @@
+package basic;
+
+public  abstract class Abc {
+	public Abc() {
+		
+	}
+
+	public abstract void getitem();{
+		
+	}
+	public static void main(String[] args){
+		//Abc abc=new abc();
+		Subabc sub=new Subabc();
+	}
+}
+class Subabc extends Abc{
+
+	@Override
+	public void getitem() {
+		
+		
+	}
+	
+}
--- operator/src/basic/PoliceCar1.java
+++ operator/src/basic/PoliceCar1.java
@@ -4,10 +4,10 @@ public class PoliceCar1 extends Car{
 	public void brake() {
 		System.out.println("car brake");
 	}
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		Car car1=new Car();
 		car1.brake();
-		System.out.println("Policecar1 instanceof car");
+		System.out.println("Policecar1");
 	}
 
 }
