c452b47c62c5572e227e9ee7c3cf246e7ea0e722
vinotha64 <vinothablossom14@gmail.com>
2018-06-07 15:18:44 +0530

    t

---
+++ operator/src/basic/Animal.java
@@ -0,0 +1,26 @@
+package basic;
+
+public abstract class Animal implements Travel {
+
+}
+class dog extends Animal{
+
+	@Override
+	public void getspeed() {
+	}
+		
+	}
+class cat extends Animal{
+
+	@Override
+	public void getspeed() {
+	}
+		
+	}
+class panda extends Animal{
+
+	@Override
+	public void getspeed() {
+	}
+		
+	}
