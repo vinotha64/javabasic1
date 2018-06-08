80e97bd5b5200d8b44d2618d1c8fb238a0772e38
vinotha64 <vinothablossom14@gmail.com>
2018-06-08 11:44:00 +0530

    test

--- operator/src/basic/Test.java
+++ operator/src/basic/Test.java
@@ -4,8 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 	Travel travel1=new bus();
 	Travel travel2=new dog();
-	System.out.println("travel1 instanceof bus");
-	System.out.println("travel2 instanceof dog");
+	System.out.println(travel1 instanceof bus);
+	System.out.println(travel2 instanceof dog);
 	Travel travel3=new panda();
 	Owner owner=(panda)travel3;
 	}
