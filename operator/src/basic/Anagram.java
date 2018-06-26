0e22bc3109540d90a73931fec636d6a4b4e1352b
vinotha64 <vinothablossom14@gmail.com>
2018-06-26 14:06:34 +0530

    an

---
+++ operator/src/basic/Anagram.java
@@ -0,0 +1,27 @@
+package basic;
+
+import java.util.Arrays;
+
+public class Anagram {
+  
+	   public static void main(String args[]) {
+	      String str1 = "vinotha";
+	      String str2 = "thainov";
+
+	      if (str1.length()==str2.length()) {
+	         char[] arr1 = str1.toCharArray();
+	         Arrays.sort(arr1);
+	         System.out.println(Arrays.toString(arr1));
+	         char[] arr2 = str2.toCharArray();
+	         Arrays.sort(arr2);
+	         System.out.println(Arrays.toString(arr2));
+	         System.out.println(Arrays.equals(arr1, arr2));
+	         if(arr1.equals(arr2)) {
+	            System.out.println("Given strings are anagrams");
+	         } else {
+	            System.out.println("Given strings are not anagrams");
+	         }
+	      }
+	   }
+	}
+
